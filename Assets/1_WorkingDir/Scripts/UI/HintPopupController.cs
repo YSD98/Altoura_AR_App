cbc49fbdc1a06904f101ae0ede7bbb25f1d2c46d
Yash Asus PC <62443679+YSD98@users.noreply.github.com>
2026-02-25 15:52:56 +0530

    Builded and Tested

--- Assets/1_WorkingDir/Scripts/UI/HintPopupController.cs
+++ Assets/1_WorkingDir/Scripts/UI/HintPopupController.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
+using System.Collections;  
 
 public class HintPopupController : MonoBehaviour
 {
-    public CanvasGroup scanPopup,PlacePopup;
+    public GameObject scanPopup, placePopup;
+
+    void Start()
+    {
+        
+        StartCoroutine(ShowHintPopups());
+    }
+
+    IEnumerator ShowHintPopups()
+    {
+        yield return new WaitForSeconds(5f);
+        scanPopup.SetActive(false);
+        placePopup.SetActive(true);
+
+        yield return new WaitForSeconds(3f);
+        placePopup.SetActive(false);
+    }
 }
